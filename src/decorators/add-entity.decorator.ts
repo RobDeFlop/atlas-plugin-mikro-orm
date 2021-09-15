832c8276d69ee52c81c342a1124f945a3fcc82b7
RobDeFlop <domeme.ks@gmail.com>
2021-09-15 14:31:05 +0200

    implemented @Entity inside @AddEntity() implemented @AddEmbeddable with @Embeddable inside

--- src/decorators/add-entity.decorator.ts
+++ src/decorators/add-entity.decorator.ts
@@ -1,21 +1,28 @@
 import { app, constructor, getFrameworkMetaData, Injectable } from '@abstractflo/atlas-shared';
 import { MikroService } from '../services/mikro.service';
 import { Symbols } from '../symbols';
+import { Entity, EntityOptions } from '@mikro-orm/core';
 
 /**
  * Decorator to register entities automatically
- * @param {constructor<any>} target
  * @constructor
+ * @param options
  */
-export function AddEntity(target: constructor<any>): void{
-  const mikroService = app.resolve(MikroService);
-  const databaseEntities = getFrameworkMetaData<constructor<any>[]>(Symbols.MIKRO_ENTITIES, mikroService);
-  const doesEntityExists = databaseEntities.find((entity: constructor<any>) => entity === target)
+export function AddEntity(options?: EntityOptions<any>): (target: constructor<any>) => void {
+  return function(target: constructor<any>){
+    const mikroService = app.resolve(MikroService);
+    const databaseEntities = getFrameworkMetaData<constructor<any>[]>(Symbols.MIKRO_ENTITIES, mikroService);
+    const doesEntityExists = databaseEntities.find((entity: constructor<any>) => entity === target);
 
-  if(doesEntityExists) return;
+    if (doesEntityExists) return false;
 
-  Injectable(target);
+    Injectable(target);
+    Entity(options)(target);
+    databaseEntities.push(target);
+    Reflect.defineMetadata<constructor<any>[]>(Symbols.MIKRO_ENTITIES, databaseEntities, mikroService);
 
-  databaseEntities.push(target);
-  Reflect.defineMetadata<constructor<any>[]>(Symbols.MIKRO_ENTITIES,databaseEntities, mikroService)
+    return target;
+  }
+
 }
+
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import './services/mikro.service';
 
 export * from '@mikro-orm/core';
 export { AddEntity } from './decorators/add-entity.decorator';
+export { AddEmbeddable } from './decorators/add-embeddable.decorator';
 export { Repository } from './repository';
 export { MikroService } from './services/mikro.service';
