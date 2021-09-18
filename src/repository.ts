75e04a9977d23c843d6dbfff39fff984beabec4b
RobDeFlop <domeme.ks@gmail.com>
2021-09-18 03:28:07 +0200

    potential fix #2 of repository.ts

--- src/repository.ts
+++ src/repository.ts
@@ -1,12 +1,12 @@
 import { MikroService } from './services/mikro.service';
 import { app } from '@abstractflo/atlas-shared';
-import { EntityRepository } from '@mikro-orm/core';
+import { AnyEntity, EntityRepository } from '@mikro-orm/core';
 
 export abstract class Repository<T> {
   protected entityType: new (...args: any[]) => T;
 
-  protected get repository(): EntityRepository<T> {
-    const mikroService = app.resolve(MikroService);
-    return mikroService.entityManager.getRepository(this.entityType);
+  protected repository(): EntityRepository<AnyEntity<unknown>> {
+    const mikroService: MikroService = app.resolve(MikroService);
+    return mikroService.entityManager.getRepository(typeof this.entityType);
   }
 }
